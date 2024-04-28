bac10f38a57f343714315aeb7746c819b32b49d3
Dorian Marques <dorian.marques.2e7@gmail.com>
2024-04-28 22:36:40 +0200

    add design for signin / signup WIP

--- src/Views/SignIn.tsx
+++ src/Views/SignIn.tsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import {defaullHeader} from "../utils/header.ts";
 import {SIGNIN} from "../utils/api.ts";
 import {useNavigate} from "react-router-dom";
+import {RiGamepadFill} from "@remixicon/react";
 
 function SignIn() {
     const [email, setEmail] = useState('');
@@ -25,12 +26,34 @@ function SignIn() {
 
     return (
         <>
-            <div className={'d-flex flex-column justify-content-center align-items-center w-50 mx-auto mt-5'}>
-                <input className={'form-control'} type="text" placeholder="mail" value={email}
-                       onChange={(e) => setEmail(e.target.value)}></input>
-                <input className={'form-control'} type="password" placeholder="password" value={password}
-                       onChange={(e) => setPassword(e.target.value)}></input>
-                <button className={'btn btn-primary'} onClick={login}>Login</button>
+            <div className="row h-100">
+                <div className="col-md-6 d-flex flex-column justify-content-center">
+                    <div className="w-50 mx-auto">
+                        <h1 className="text-center mb-2">Connexion</h1>
+                        <p className="text-center mb-4">Connectez vous avec votre compte</p>
+                        <input className="form-control mb-3" type="text" placeholder="mail" value={email}
+                               onChange={(e) => setEmail(e.target.value)}></input>
+                        <input className="form-control mb-3" type="password" placeholder="password" value={password}
+                               onChange={(e) => setPassword(e.target.value)}></input>
+                        <button className="btn btn-primary w-100 text-white mb-3" onClick={login}>Login</button>
+                        <p className="mt-3 small-text text-center">Si vous n'avez pas de compte cliquez <a
+                            href="/signup">ici</a> pour en créer
+                            un</p>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                    <div
+                        className="card position-relative card-inner h-75 w-75 d-flex justify-content-center align-items-center">
+                        <div
+                            className="position-absolute mx-auto card card-glass w-75">
+                            <p className="text-center text-white font-jersey fs-2">
+                                Plongez dans un univers épique avec notre jeu où chaque
+                                mission est une chance de découvrir des trésors inédits
+                                <RiGamepadFill size={40} color="white" className="ms-4"></RiGamepadFill>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
--- src/Views/SignUp.tsx
+++ src/Views/SignUp.tsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 import {defaullHeader} from "../utils/header.ts";
 import {SIGNUP} from "../utils/api.ts";
 import {useNavigate} from "react-router-dom";
+import {RiGamepadFill} from "@remixicon/react";
 
 function SignUp() {
     const [name, setName] = useState('');
@@ -24,14 +25,38 @@ function SignUp() {
 
     return (
         <>
-            <div className={'d-flex flex-column justify-content-center align-items-center w-50 mx-auto mt-5'}>
-                <input className={'form-control'} type="text" placeholder="name" value={name}
-                       onChange={(e) => setName(e.target.value)}></input>
-                <input className={'form-control'} type="text" placeholder="mail" value={email}
-                       onChange={(e) => setEmail(e.target.value)}></input>
-                <input className={'form-control'} type="password" placeholder="password" value={password}
-                       onChange={(e) => setPassword(e.target.value)}></input>
-                <button className={'btn btn-primary'} onClick={signup}>S'inscrire</button>
+            <div className="row h-100">
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                    <div
+                        className="card position-relative card-inner h-75 w-75 d-flex justify-content-center align-items-center">
+                        <div
+                            className="position-absolute mx-auto card card-glass w-75">
+                            <p className="text-center text-white font-jersey fs-2">
+                                Plongez dans un univers épique avec notre jeu où chaque
+                                mission est une chance de découvrir des trésors inédits
+                                <RiGamepadFill size={40} color="white" className="ms-4"></RiGamepadFill>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex flex-column justify-content-center">
+                    <div className="w-50 mx-auto">
+                        <h1 className="text-center mb-2">S'inscrire</h1>
+                        <p className="text-center mb-4">Inscrivez vous pour commencer a jouer !</p>
+                        <input className=" form-control mb-3" type="text" placeholder="name" value={name}
+                               onChange={(e) => setName(e.target.value)}></input>
+
+                        <input className="form-control mb-3" type="text" placeholder="mail" value={email}
+                               onChange={(e) => setEmail(e.target.value)}></input>
+
+                        <input className="form-control mb-3" type=" password" placeholder=" password" value={password}
+                               onChange={(e) => setPassword(e.target.value)}></input>
+                        <button className=" btn btn-secondary w-100 text-white mb-3" onClick={signup}>S' inscrire
+                        </button>
+                        <p className="mt-3 small-text text-center">Si vous avez déjà un compte cliquez <a
+                            href="/signin">ici</a> pour vous connecter</p>
+                    </div>
+                </div>
             </div>
         </>
     )
